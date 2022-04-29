640718ead2e846ed9a2f201a9e7c262dff073d44
XimeAlpizar <0225683@up.edu.mx>
2022-04-29 12:33:34 -0500

    Actualizacion

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import  Colors from '../constants/colors'
 
 const Header = ({ title }) => {
     return (
        <View style={styles.header}>
            <Text style={styles.headerTitle}>{title}</Text>
        </View>
-    )
+    );
 }
 
 const styles = StyleSheet.create({
@@ -13,10 +15,9 @@ const styles = StyleSheet.create({
         width: '100%',
         height: 90,
         paddingTop: 36,
-        backgroundColor: '#f7287b',
+        backgroundColor: Colors.primary,
         alignItems: 'center',
         justifyContent: 'center'
-
     },
     headerTitle: {
         color: 'black',
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,22 +1,82 @@
-import React from 'react'
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-const StartGameScreen = () => {
+import Card from '../components/Card'
+import Colors from '../constants/Colors';
+import Input from '../components/Input';
+import NumberContainer from '../components/NumberContainer';
+
+function StartGameScreen () {
+
+const [enteredValue, setEnteredValue] = useState('');
+const [confirmed, setConfirmed] = useState(false);
+const [selectedNumber, setSelectedNumber] = useState(undefined);
+
+const numberInputHandler = input => {
+    setEnteredValue(input.replace(/[^0-9]/g, ''));
+}
+
+const resetInputHandler = input => {
+    setEnteredValue('')
+    setConfirmed(false)
+}
+
+const confirmInputHandler = () => {
+    const chosenNumber = parseInt(enteredValue);
+    if(isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) return
+
+    setConfirmed(true)
+    setSelectedNumber(chosenNumber)
+    setEnteredValue('')
+}
+
+let confirmedOutput;
+
+if(confirmed){
+    confirmedOutput = (
+    <Card style={styles.selectedContainer}>
+        <Text>You selected:</Text>
+        <NumberContainer>
+            {selectedNumber}
+        </NumberContainer>
+    </Card>
+    )
+}
+
   return (
     <View style={styles.screen}>
-        <Text style={styles.title}>Select a Number</Text>
-        <View style={styles.inputContainer}>
-            <TextInput />
-            <View style={styles.buttonContainer}>
-                <Button style={styles.button} title="Reset"/>
-                <Button style={styles.button} title="Confirm"/>
+        <Card>
+            <Text style={styles.title}>Select a Number</Text>
+                <Input 
+                style={styles.input}
+                blurOnSubmit//Android
+                autoCapitalize='none'
+                autoCorrect={false}
+                keyboardType="number-pad"
+                maxLength={2}
+                onChangeText={numberInputHandler}
+                value={enteredValue}
+                />
+                <View style={styles.buttonContainer}>
+                <View style={styles.button}>
+                    <Button style={styles.button} title="Reset" color={Colors.secondary} onPress={resetInputHandler}/>
+                    </View>
+                    <View style={styles.button}>
+                    <Button style={styles.button} title="Confirm" color={Colors.primary} onPress={confirmInputHandler}/>
+                </View>
             </View>
-        </View>
+        </Card>
+        {confirmedOutput}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
+    selectedContainer:{
+        marginTop: 20,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
     screen: {
         flex: 1,
         padding: 10,
@@ -41,13 +101,18 @@ const styles = StyleSheet.create({
         borderRadius: 10
     },
     button:{
-
+       width: 100
     },
     buttonContainer:{
         flexDirection: 'row',
-        flex: 1
-
+        width: '100%',
+        justifyContent: 'space-between',
+        paddingHorizontal: 15,
+    },
+    input:{
+        width: 50,
+        textAlign: 'center'
     }
   });
 
-export default StartGameScreen
+export default StartGameScreen;
